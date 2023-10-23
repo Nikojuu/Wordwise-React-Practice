92fc397097f493819a324390115c9b5d626a4713
Nikojuu <come_on12345@hotmail.com>
2023-10-23 19:03:42 +0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import Appnav from "./Appnav";
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <Appnav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
